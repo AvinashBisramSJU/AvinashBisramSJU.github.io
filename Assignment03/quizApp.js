acb2626f2af6b022a2352c24903affc740393e66
AvinashBisramSJU <avinash.bisram19@stjohns.edu>
2021-11-07 20:33:07 -0500

    Batch Commit

--- Assignment03/quizApp.js
+++ Assignment03/quizApp.js
@@ -27,9 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Event Delegation Handler for any button press within the main quiz screen
 async function handle_button_click(e) {
-    console.clear();
-    console.log("Widget interaction registered in");
-    console.log(e.target);
     if (appState.current_view == "#intro_view") {
         if (e.target.dataset.action == "start") {
             // Set current question to 0
@@ -53,12 +50,12 @@ async function handle_button_click(e) {
             
             // Access the api to get the right model
             appState.current_model = await getModel(final_url,appState);
-            console.log(appState);
             setQuestionView(appState);
             update_view(appState);
 
-            // change page container size to reveal scoreboard
+            // Change page container size to reveal scoreboard
             showScoreBoard();
+            document.querySelector("#elapsed_time_display").querySelector("span").innerHTML = 0;
             timerInterval = setInterval(async()=>{
                 elapsed_time++;
                 document.querySelector("#elapsed_time_display").querySelector("span").innerHTML = `${elapsed_time}`;  
@@ -71,7 +68,6 @@ async function handle_button_click(e) {
         if (e.target.dataset.action == "answer") {
             // Store their answer
             user_response = e.target.dataset.answer;
-            console.log("Selected answer: "+user_response);
 
             // Enable the submit button
             submit_button = document.querySelector('button[data-action="submit"]');
@@ -81,16 +77,11 @@ async function handle_button_click(e) {
         }
         if (e.target.dataset.action == "submit") {
             
-            // Temporary code for now
             check_user_response(user_response,appState.current_model)
         }
     }
 
     if (appState.current_view == "#multiple_choice_view") {
-        console.clear();
-        console.log("multiple choice interaction registered");
-        console.log("The interaction target was");
-        console.log(e.target);
         if (e.target.type == "radio") {
             user_response = e.target.value;
 
@@ -100,13 +91,12 @@ async function handle_button_click(e) {
             }
         }
         if (e.target.dataset.action == "submit") {
-            // Temporary code for now
+
             check_user_response(user_response,appState.current_model)
         }
     }
 
     if (appState.current_view == "#text_input_view") {
-        console.log("text input interaction registered")
         // Keep the submit button disabled until the user types something
         if (e.target.type == "text") {
             submit_button = document.querySelector('button[data-action="submit"]');
@@ -118,13 +108,11 @@ async function handle_button_click(e) {
 
             user_response = document.querySelector('input[name="answer"]').value;
 
-            // Temporary code for now
             check_user_response(user_response,appState.current_model)
         }
     }
 
     if (appState.current_view == "#fill_blank_view") {
-        console.log("fill blank interaction registered");
         if (e.target.type == "text") {
             submit_button = document.querySelector('button[data-action="submit"]');
             if (submit_button.disabled == true) {
@@ -135,13 +123,11 @@ async function handle_button_click(e) {
             
             user_response = document.querySelector('input[type="text"]').value;
 
-            // Temporary code for now
             check_user_response(user_response,appState.current_model)
         }
     }
 
     if (appState.current_view == "#fix_error_view") {
-        console.log("fix error interaction registered");
         if (e.target.type == "text") {
             submit_button = document.querySelector('button[data-action="submit"]');
             if (submit_button.disabled == true) {
@@ -152,13 +138,11 @@ async function handle_button_click(e) {
 
             user_response = document.querySelector('input[type="text"]').value;
 
-            // Temporary code for now
             check_user_response(user_response,appState.current_model)
         }
     }
 
     if (appState.current_view == "#feedback_view") {
-        console.log("feedback interaction registered");
         if (e.target.dataset.action == "gotIt") {
             updateQuestion(appState);
         }
@@ -166,21 +150,20 @@ async function handle_button_click(e) {
     }
 
     if (appState.current_view == "#end_view") {
-        console.log("end view interaction registered");
-        console.log(`Stored name: ${user_fname} ${user_lname}`);
-        console.log(`Correct Answers: ${appState.correctAnswers}/${appState.totalAnswers}`);
         if (e.target.dataset.action == "retake") {
-            /*
-            Set current_question to 0 and update_view
-            Reset the counts for total questions answered and correct answers given
-            */
             appState.current_question = 0;
             appState.correctAnswers = 0;
             appState.totalAnswers = 0;
             appState.current_model = await getModel(appState.current_quiz,appState);
             setQuestionView(appState);
             update_view(appState);
-            update_counters();
+            update_counters(appState);
+
+            document.querySelector("#elapsed_time_display").querySelector("span").innerHTML = elapsed_time = 0;
+            timerInterval = setInterval(async()=>{
+                elapsed_time++;
+                document.querySelector("#elapsed_time_display").querySelector("span").innerHTML = `${elapsed_time}`;  
+            },1000);
         }
         if (e.target.dataset.action == "return") {
             window.location.reload();
@@ -200,21 +183,18 @@ async function getModel(url,appState) {
 
 // Checks if user_response is correct and displays appropriate feedback screen.
 function check_user_response(user_answer, model) {
-    console.log(`Checking response...\nAnswer given: ${user_response}\nCorrect Answer: ${appState.current_model.correctAnswer}`);
     
     if (user_answer == model.correctAnswer) {
-        console.log("You answered correct!")
-        /* alert("Correct!"); */
         appState.correctAnswers++;
         document.querySelector("#page_container").innerHTML = `<h1 style="color:green">Correct!<h1>`;
         setTimeout(()=> {
             updateQuestion(appState);
-        }, 500);
+        }, 1000);
     }
     else {
-        console.log(`You answered incorrectly.\nExplanation: ${appState.current_model.explanation}`);
         appState.current_view = "#feedback_view";
         update_view(appState);
+        document.querySelector("#wrong_answer").innerHTML = `Your Answer: ${user_answer}`;
     }
     appState.totalAnswers++;
     update_counters(appState);
@@ -222,11 +202,8 @@ function check_user_response(user_answer, model) {
 
 // Increments the current_question and current_model
 async function updateQuestion(appState) {
-    console.log(`URL being accessed: ${appState.current_quiz}`)
     const response = await fetch(appState.current_quiz);
     const result = await response.json();
-    console.log("Current Question Index: "+appState.current_question);
-    console.log("Last Question Index: "+(result.length-1));
     if (appState.current_question < (result.length-1)) {
         appState.current_question = appState.current_question + 1;
         appState.current_model = await getModel(appState.current_quiz,appState);
@@ -275,7 +252,7 @@ function update_view(appState) {
             document.querySelector("#completion_text").innerHTML = `Congratulations ${user_fname} ${user_lname}!\nYou passed the quiz with a score of ${correctPercent}%!`
         }
         else {
-            document.querySelector("#completion_text").innerHTML = `Sorry ${user_fname} ${user_lname}, you did not pass.\nYour final score was a ${correctPercent}%`
+            document.querySelector("#completion_text").innerHTML = `Sorry ${user_fname} ${user_lname}, you fail the quiz.\nYour final score was a ${correctPercent}%`
         }
     }
 }
